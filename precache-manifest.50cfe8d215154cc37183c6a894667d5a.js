79e20badf1e834802f7de6369c71f9cd877fac72
nkanor <janeetim248@gmail.com>
2022-08-02 21:45:06 +0100

    Updates

---
+++ precache-manifest.50cfe8d215154cc37183c6a894667d5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ba28c0c49de7c3cd9c1",
+    "url": "/static/css/main.046126f0.chunk.css"
+  },
+  {
+    "revision": "8ba28c0c49de7c3cd9c1",
+    "url": "/static/js/main.8ba28c0c.chunk.js"
+  },
+  {
+    "revision": "8e07af9dc216129809a3",
+    "url": "/static/js/1.8e07af9d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "129aa3cca0dee7250f056bd7340e0e77",
+    "url": "/index.html"
+  }
+];
